19333c9bea2b85ecd3c389bd3b796e7d3eded3e0
anandKmrSharma <87422058+anandKmrSharma@users.noreply.github.com>
2022-10-09 23:43:46 +0530

    updated

---
+++ weatherapplication/src/api.jsx
@@ -0,0 +1,33 @@
+
+
+
+
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+		'X-RapidAPI-Key': 'd36479ec1bmsh64df2967efa2ea8p1b0468jsn2bc46f1a4c10',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "22587b441a9805af596fe933a8ee78bc";
+
+
+
+
+
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'd36479ec1bmsh64df2967efa2ea8p1b0468jsn2bc46f1a4c10',
+// 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
